f3e232ab0140b8fa925c0c65533c0682b2504f0e
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2015-11-03 11:16:38 -0200

    create home page view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <title>SAC</title>
+
+    <link rel="stylesheet" href="lib/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <span class="navbar-brand">SAC</span>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li class="active"><a href="#">Chamados</a></li>
+          <li><a href="#">Cadastrar</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      <h1>Chamados</h1>
+      <hr>
+    </div>
+  </body>
+</html>
