e348496036ef3d0b80ea2473b396826b5393d36b
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2015-11-03 20:31:24 -0200

    add orderby

--- app/Http/Controllers/ChamadosController.php
+++ app/Http/Controllers/ChamadosController.php
@@ -10,13 +10,69 @@ use Illuminate\Http\Response;
 
 class ChamadosController extends Controller {
 
-    public function getChamados()
+    public function getChamados(Request $request)
     {
-        $chamados = Chamado::paginate(5);
+        $orders = [
+            'pedido' => 'pedidos.id',
+            'email'  => 'clientes.email',
+        ];
+
+        $orderby = (isset($request['orderby']))
+            ? $orders[$request['orderby']]
+            : $orders['pedido'];
+
+        $order = (isset($request['order']))
+            ? $request['order']
+            : 'asc';
+
+        $pedidoFiltro = '?orderby=pedido&order=';
+        $emailFiltro  = '?orderby=email&order=';
+
+        $pedidoFiltroIcone = 'bottom';
+        $emailFiltroIcone  = 'bottom';
+
+        if ($request['orderby'] === 'pedido') {
+            if ($order === 'asc') {
+                $pedidoFiltro      .= 'desc';
+                $pedidoFiltroIcone  = 'top';
+            } else {
+                $pedidoFiltro      .= 'asc';
+                $pedidoFiltroIcone  = 'bottom';
+            }
+        } else {
+            $pedidoFiltro .= 'asc';
+        }
+
+        if ($request['orderby'] === 'email') {
+            if ($order === 'asc') {
+                $emailFiltro      .= 'desc';
+                $emailFiltroIcone  = 'top';
+            } else {
+                $emailFiltro      .= 'asc';
+                $emailFiltroIcone  = 'bottom';
+            }
+        } else {
+            $emailFiltro .= 'asc';
+        }
+
+        $chamados = Chamado::select('chamados.*')
+            ->join('pedidos', 'pedidos.id', '=', 'chamados.id_pedido')
+            ->join('clientes', 'clientes.id', '=', 'chamados.id_cliente')
+            ->orderby($orderby, $order)
+            ->paginate(3);
+
+        $paginacao = $chamados->appends([
+            'orderby' => (isset($request['orderby'])) ? $request['orderby'] : 'pedido',
+            'order'   => (isset($request['order'])) ? $request['order'] : 'asc',
+        ])->render();
 
         return view('chamados')
             ->with('chamados', $chamados)
-            ->with('paginacao', $chamados->render());
+            ->with('paginacao', $paginacao)
+            ->with('pedidoFiltro', $pedidoFiltro)
+            ->with('pedidoFiltroIcone', $pedidoFiltroIcone)
+            ->with('emailFiltroIcone', $emailFiltroIcone)
+            ->with('emailFiltro', $emailFiltro);
     }
 
     public function getChamado($id)
