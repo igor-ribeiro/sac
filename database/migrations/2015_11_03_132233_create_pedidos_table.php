ff9fba3ae4340deed0aa4f30149a7bde6393fdb9
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2015-11-03 11:31:42 -0200

    create pedidos table migration

---
+++ database/migrations/2015_11_03_132233_create_pedidos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_cliente')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('id_cliente')->references('id')->on('clientes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pedidos');
+    }
+}
