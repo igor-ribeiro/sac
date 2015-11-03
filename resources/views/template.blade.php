26a8a725eaf216359029392dbc6318dae82f66d9
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2015-11-03 13:36:20 -0200

    add correct url to nav links and add bootstrap theme

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,6 +5,7 @@
         <title>SAC</title>
 
         <link rel="stylesheet" href="lib/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="lib/bootstrap/dist/css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="css/app.css">
     </head>
     <body>
@@ -13,8 +14,8 @@
                 <span class="navbar-brand">SAC</span>
 
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="#">Chamados</a></li>
-                    <li><a href="#">Cadastrar</a></li>
+                    <li><a class="js-nav-link" href="{{ route('chamados') }}">Chamados</a></li>
+                    <li><a class="js-nav-link" href="{{ route('cadastrar') }}">Cadastrar</a></li>
                 </ul>
             </div>
         </nav>
