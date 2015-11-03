01f3f23da4ce1137b9fc85b92390d72b7e569603
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2015-11-03 17:49:35 -0200

    add button for details

--- resources/views/chamados.blade.php
+++ resources/views/chamados.blade.php
@@ -9,6 +9,7 @@
     @else
         <table class="table table-striped table-hover">
             <thead>
+                <th></th>
                 <th>Nº Pedido</th>
                 <th>E-Mail</th>
                 <th>Título</th>
@@ -16,6 +17,7 @@
             <tbody>
                 @foreach ($chamados as $chamado)
                     <tr>
+                        <td><a href="#/chamado/{{ $chamado->id }}" class="btn btn-default glyphicon glyphicon-search"></a></td>
                         <td>{{ $chamado->pedido->id }}</td>
                         <td>{{ $chamado->cliente->email }}</td>
                         <td>{{ $chamado->titulo }}</td>
