751a41b283ff6164a19b175e931333bd07cbd774
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2015-11-03 17:38:59 -0200

    create postCadatrarChamado

--- app/Http/Controllers/ChamadosController.php
+++ app/Http/Controllers/ChamadosController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chamado;
+use App\Models\Cliente;
+use App\Models\Pedido;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ChamadosController extends Controller {
 
@@ -18,6 +22,50 @@ class ChamadosController extends Controller {
     public function getCadastrarChamado()
     {
         return view('cadastrar');
+    }
+
+    public function postCadastrarChamado(Request $request)
+    {
+        $this->validate($request, [
+            'pedido.numero'  => 'required|exists:pedidos,id|',
+            'cliente.email'  => 'required|email',
+            'chamado.titulo' => 'required',
+        ]);
+
+        $cliente = Cliente::firstOrNew([
+            'email' => $request->cliente['email'],
+        ]);
+
+        $pedido = Pedido::find($request->pedido['numero']);
+
+        // se o cliente não existir, verifica se um nome
+        // foi fornecido e cadastra o clinte com o nome
+        if (!isset($cliente->id)) {
+            if (empty($request->cliente['nome'])) {
+                $this->validate($request, [
+                    'cliente.nome' => 'required',
+                ]);
+            } else {
+                $cliente->nome = $request->cliente['nome'];
+            }
+
+            if (!$cliente->save()) {
+                return response('Erro ao salvar o cliente: ' . $cliente->nome, 400);
+            }
+        }
+
+        $chamado = Chamado::create([
+            'id_cliente' => $cliente->id,
+            'id_pedido'  => $pedido->id,
+            'titulo'     => $request->chamado['titulo'],
+            'observacao' => $request->chamado['observacao'],
+        ]);
+
+        if (!$chamado->save()) {
+            return response('Erro ao salvar o chamado', 400);
+        }
+
+        return response('Chamado cadastrado com sucesso!', 200);
     }
 
 }
