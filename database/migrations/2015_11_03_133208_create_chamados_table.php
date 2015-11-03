c167c97381b8b7075aac112be9a248f8094b7b2e
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2015-11-03 11:34:55 -0200

    create chamados table migration

---
+++ database/migrations/2015_11_03_133208_create_chamados_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChamadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chamados', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_pedido')->unsigned();
+            $table->string('titulo');
+            $table->text('observacao');
+            $table->timestamps();
+
+            $table->foreign('id_pedido')->references('id')->on('pedidos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('chamados');
+    }
+}
